8c5e94ec4fe260e7e33d49d362b1b508d99e52a4
Doeunnkimm <doeunnkimm@gmail.com>
2023-03-15 21:08:41 +0900

    :sparkles: Feat : 검색으로 조회 기능 구현
    + react-markdown, remark-gfm 라이브러리 의존성 추가

--- src/Pages/List/index.js
+++ src/Pages/List/index.js
@@ -1,9 +1,13 @@
-import { useEffect } from 'react';
+import styled from '@emotion/styled';
+import { useCallback, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { IssuesAPI } from '../../Apis/issues';
 import { searchActions } from '../../Stores/search';
+import IssueCard from './Card/Card';
 
 function ListPage() {
+  const [issues, setIssues] = useState([]);
   const { owner, repository, page, sort, perPage } = useParams();
   const dispatch = useDispatch();
 
@@ -13,10 +17,44 @@ function ListPage() {
     );
   }, []);
 
+  const getData = useCallback(async () => {
+    try {
+      const { data } = await IssuesAPI.getData(owner, repository, {
+        page,
+        sort,
+        perPage,
+      });
+      setIssues(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, [page, sort, perPage]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
   return (
-    <div>
-      <p>:D</p>
-    </div>
+    <S.Wrapper>
+      {issues.map((issue) => (
+        <IssueCard
+          number={`🌎 ${issue.number}`}
+          title={issue.title}
+          body={
+            issue.body
+              ? issue.body.split('').slice(0, 99).join('') + ' ...'
+              : issue.body
+          }
+        />
+      ))}
+    </S.Wrapper>
   );
 }
 export default ListPage;
+
+const Wrapper = styled.div`
+  width: 60%;
+  margin: 20px auto;
+`;
+
+const S = { Wrapper };
--- src/Styles/global.js
+++ src/Styles/global.js
@@ -19,6 +19,7 @@ const globalStyles = css`
   :root {
     --color-dark-white: #f9f7f7;
     --color-pink: #f2d7d9;
+    --color-light-purple: #eae8f1;
     --color-purple: #d3cedf;
     --color-light-blue: #cfe3f7;
     --color-blue: #9cb4cc;
